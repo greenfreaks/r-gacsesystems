4081a32bbeb8e23371c54b1d3784dfaf5445fc5d
Mario Sandoval Velázquez <mariosandovalv1998@gmail.com>
2022-05-20 13:34:02 -0500

    Cards de citas creadas dinámicamente con PHP

--- php/citasAgendadas.php
+++ php/citasAgendadas.php
@@ -1,17 +1,32 @@
 <?php
 include "connection.php";
-function citasPendientes(){
-    include "connection.php";
-    $result = array();
-    $result ['error'] = false;
-    $result ['mensaje'] = "";
-    $result['citasPendientes'] =
-    $citas = [];
-    $citas_query = "SELECT * FROM citas WHERE fk_cita_estatus =  1 AND fk_cita_estatus =  2 AND fk_cita_estatus =  3";
-    $citas_execute = mysqli_query($con, $citas_query);
-    while($row_citas = $citas_execute->fetch_assoc()){
-        $cita = array();
-        $cita['id_cita'] = $row_citas['id_cita'];
+$query_citas = "SELECT * FROM `citas` INNER JOIN cita_estatus ce ON citas.fk_cita_estatus = ce.id_estatus 
+WHERE fk_medico = '$id_medico'; ";
+$result_citas = mysqli_query($con, $query_citas);
+$total_citas = mysqli_num_rows($result_citas);
+if($total_citas >= 1){
+    while($citas = mysqli_fetch_assoc($result_citas)){?>
+        <div class="card">
+            <div class="cardHeader">
+                <div class="cardTitle">
+                    <h4 class="orangeTx"><?php echo $citas['nombre_paciente'] . " " . $citas["apellido_paciente"];?></h4>
+                </div>
+                <div class="cardBody">
+                    <p class="cardBody__observaciones"><?php echo $citas['observaciones']?></p>
+                    <p><b> Fecha de cita: </b><?php echo $citas['fecha_cita'];?></p>
+                    <p><b> Hora: </b><?php echo $citas["hora_cita"];?></p>
+                    <p><b> Estatus: </b><?php echo $citas["nombre_estatus"];?></p>
+                </div>
+        
+                <div class="cardFooter">
+                </div>
+            </div>
+        </div>
+        <?php
     }
+}else{?>
+<h1 class="noCitas">Aún no tienes citas agendadas</h1>
+<?php
 }
+
 ?>

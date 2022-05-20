5517ffb71d4477a5995e515db2a7d9bcc13bdec7
Mario Sandoval Velázquez <mariosandovalv1998@gmail.com>
2022-05-20 10:48:20 -0500

    Se crea plantilla de navbar y se introduce ahí el nombre del médico registrado

--- sections/agendar.php
+++ sections/agendar.php
@@ -37,26 +37,7 @@ else{
 </head>
 
 <body>
-    <!-- <header class="header">
-        <nav id="navHome" class="header__nav boxShadow bgBlack">
-            <h1 class="orangeTx">Citas Próximas</h1>
-        </nav>
-    </header> -->
-    <div class="sidebarBurgerBtn">
-        <i class="fas fa-bars"></i>
-    </div>
-    <div class="sidebar">
-        <img src="../assets/img/index-logo-min.png" class="alignCenter" alt="">
-        <div class="text whiteTx">
-            <h4>Doctor/a: </h4>
-        </div>
-        <ul class="noList">
-            <li><a href="home.php" class="orangeTx">Citas próximas</a></li>
-            <li><a href="#">Crear Cita</a></li>
-            <li><a href="#">Ver registro de citas</a></li>
-            <li><a href="../php/logout.php">Cerrar sesión</a></li>
-        </ul>
-    </div>
+    <?php include "../view-templates/sidenav.php"?>
     <div class="appBodyContent">
         <section class="agendar">
             <form id="agendarForm" class="agendarForm boxShadow">
--- sections/home.php
+++ sections/home.php
@@ -42,22 +42,7 @@ else{
             <h1 class="orangeTx">Citas Próximas</h1>
         </nav>
     </header> -->
-  <div class="sidebarBurgerBtn">
-    <i class="fas fa-bars"></i>
-  </div>
-  <div class="sidebar">
-    <img src="../assets/img/index-logo-min.png" class="alignCenter" alt="">
-    <div class="text whiteTx">
-      <h6>Médico:</h6>
-      <h5><?php echo $user['name_medico']?></h5>
-    </div>
-    <ul class="noList">
-      <li><a href="#" class="orangeTx">Citas próximas</a></li>
-      <li><a href="agendar.php">Crear Cita</a></li>
-      <li><a href="#">Ver registro de citas</a></li>
-      <li><a href="../php/logout.php">Cerrar sesión</a></li>
-    </ul>
-  </div>
+  <?php include "../view-templates/sidenav.php"?>
   <div class="appBodyContent">
     <section class="cardSection">
       <div class="card">
